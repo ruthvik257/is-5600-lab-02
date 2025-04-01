18993c112902650811bc5e86826d756c5a5d9c2b
Ruthvik Chiniwal <61155739+ruthvik257@users.noreply.github.com>
2025-04-01 19:57:08 +0000

    Initial Submission for lab

--- app.js
+++ app.js
@@ -41,19 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
       
       generateUserList(newUser, stocksData);
       
-      // for (let i=0; i<userData.length; i++) {
-          
-      //     if (userData[i].id == id) {
-  
-      //         userData[i].user.firstname = document.querySelector('#firstname').value;
-      //         userData[i].user.lastname = document.querySelector('#lastname').value;
-      //         userData[i].user.address = document.querySelector('#address').value;
-      //         userData[i].user.city = document.querySelector('#city').value;
-      //         userData[i].user.email = document.querySelector('#email').value;     
-  
-      //         generateUserList(userData, stocksData);
-      //     } 
-      // }
+      
     });
 
 
